c4e920f7418f326aeee8bd00f6dabc908e2d780c
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2023-03-25 17:25:23 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -23,7 +23,7 @@ async def add_user(user_id: int, bot_username: str = False):
 
 
 async def full_userbase():
-    return user_data.find().count_documents({})
+    return user_data.find()
 
 
 async def del_user(user_id: int):
